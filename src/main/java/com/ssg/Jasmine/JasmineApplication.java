7b9ee467bdbbec41504db2367442c2985d8a4bf2
choiyuna <choiyuna0209@gmail.com>
2022-05-09 17:00:26 +0900

    Initial commit

---
+++ src/main/java/com/ssg/Jasmine/JasmineApplication.java
@@ -0,0 +1,14 @@
+package com.ssg.Jasmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasmineApplication.class, args);
+		System.out.println("hello");
+	}
+
+}

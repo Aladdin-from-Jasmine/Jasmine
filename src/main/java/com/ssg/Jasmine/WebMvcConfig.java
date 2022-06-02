bc74f0a0e8d6cb6307b2a6382242d49beb275d55
yeonlim <yeonlimj@gmail.com>
2022-06-02 21:16:08 +0900

    action jsp 수정

--- src/main/java/com/ssg/Jasmine/WebMvcConfig.java
+++ src/main/java/com/ssg/Jasmine/WebMvcConfig.java
@@ -34,6 +34,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 		registry.addViewController("/user/register").setViewName("user/register");
 		registry.addViewController("/user/mypage").setViewName("user/mypage");
+		
+		//aution
+		registry.addViewController("/aution/aution_list").setViewName("aution/aution_list");
 	}
 	
 //	@Override
